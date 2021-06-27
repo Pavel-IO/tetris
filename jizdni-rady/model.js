ed990a4415c14e152597753f47af5e2c573a64cc
Pavel Riha <pavel.riha@ceitec.muni.cz>
2021-06-27 11:27:43 +0200

    Finish app jizdni rady.

--- jizdni-rady/model.js
+++ jizdni-rady/model.js
@@ -1,27 +1,99 @@
+
+// ted prakticky k nicemu, zapozdruje jen jmeno zastavky, lepsi pojmenovani by bylo Station
 class Stop {
-  constructor(name, time) {
+  constructor(name) {
     this.name = name
-    this.time = time
   }
 }
 
 // Definice jizniho radu, jak jezdi spoje
+// seznam zastavek, vzdalenosti mezi nimi a casy, kdy, vyjizdi spoje
 class Line {
-  constructor(name, stops) {
+  constructor(name, stations) {
     this.name = name
-    this.stops = stops
+    this.stations = stations
+    this.distances = []
+    this.forward = []
+    this.backward = []
   }
 
-  getStops() {
-    return this.stops
+  getStations() {
+    return this.stations
   }
 
-  add(stop) {
-    this.stops.push(stop)
+  add(station) {
+    this.stations.push(station)
+  }
+
+  setDistances(distances) {
+    if (this.stations.length - 1 != distances.length) {
+      alert('Vzdalenosti neodpovídají počtu stanic.')
+    }
+    this.distances = distances
+  }
+
+  addForward(time) {
+    this.forward.push(time)
+  }
+
+  addBackward(time) {
+    this.backward.push(time)
+  }
+
+  getStations() {
+    return this.stations
+  }
+
+  findStation(name) {
+    let index = -1
+    for (let indexStop in this.stations) {
+      if (this.stations[indexStop].name == name) {
+        index = indexStop
+        break
+      }
+    }
+    if (index < 0) {
+      alert(`Station ${name} was not found in ${this.stations}.`)
+    }
+    return index
+  }
+
+  sumTime(indexFrom, indexTo) {
+    let cum = 0
+    if (indexFrom > indexTo) {
+      [indexFrom, indexTo] = [indexTo, indexFrom]
+    }
+    for (let k = indexFrom; k < indexTo; k++) {
+      cum += this.distances[k]
+    }
+    return cum
+  }
+
+  // TODO: nehleda pres pulnoc
+  findDepartures(from, to, time) {
+    let indexFrom = this.findStation(from)
+    let indexTo = this.findStation(to)
+    let departs = []
+    let processDeparts = (source, indexStartLine) => {
+      for (let lineDepart of source) {
+        let stationDepart = lineDepart + this.sumTime(0, indexFrom)
+        if (stationDepart > time) {
+          let totalTime = this.sumTime(indexFrom, indexTo)
+          departs.push(new Segment(stationDepart, totalTime))
+        }
+      }
+    }
+    if (indexFrom < indexTo) {
+      processDeparts(this.forward, 0)
+    } else {
+      processDeparts(this.backward, this.stations.length - 1)
+    }
+    return departs
   }
 }
 
-// konkretni usek cesty pri hledani spojeni mezi misty
+// konkretni usek cesty na jedne lince pri hledani spojeni mezi misty
+// je to vlastne cast linky ohranicena krajnimi stanicemi
 class DirectLine {
   constructor(line, begin) {
     this.line = line
@@ -41,6 +113,10 @@ class DirectLine {
     this.destination = destination
   }
 
+  lineName() {
+    return this.line.name
+  }
+
   clone() {
     let newDirectLine = new DirectLine(undefined, undefined)
     newDirectLine.line = this.line
@@ -54,11 +130,27 @@ class DirectLine {
   }
 }
 
-// mozny usek cesty mezi dvema, sklada se z vice linek
+// Konkretni casovy interval, navazany na DirectLine a konkretni cas
+class Segment {
+  constructor(departure, totalTime) {
+    this.departure = departure
+    this.arrival = departure + totalTime
+    this.totalTime = totalTime
+  }
+
+  assignDirectLine(directLine) {
+    this.directLine = directLine
+  }
+}
+
+// Konkretni geograficke spojeni mezi dvema stanicemi, sklada se z vice linek
+// Udrzuje pouze geografickou informaci (nikoliv casovou), po prohledani grafu
+// mam jednu instanci Route pro kazde mozne spojeni mezi dvema misty
 class Route {
   constructor(line, station) {
     this.stations = [station]
     this.transfers = [new DirectLine(line, station)]
+    this.totalTime = 0  // neni clonovano, zalezi na case, zatim nedoreseno
   }
 
   containStation(station) {
@@ -73,12 +165,16 @@ class Route {
     return this.stations.length == 1
   }
 
+  getDirectLine(index) {
+    return this.transfers[index]
+  }
+
   getLastLine() {
     return this.transfers[this.transfers.length - 1].line
   }
 
   str() {
-    return this.transfers.map((direct) => direct.str()).join(', ')
+    return this.transfers.map((direct) => direct.str()).join(' &rarr; ') + '<br>'
   }
 
   clone() {
@@ -104,9 +200,73 @@ class Route {
     }
     return this
   }
+
+  // meznik, nahoru se resi geograficke hledani, dolu se resi casove hledani
+
+  segmentsForInit(initSegment) {
+    let parts = []
+    let last = initSegment
+    last.assignDirectLine(this.transfers[0])
+    parts.push(last)
+    for (let k = 1; k < this.transfers.length; k++) {
+      let segments = this.transfers[k].line.findDepartures(this.transfers[k].begin, this.transfers[k].destination, last.arrival)
+      if (segments.length == 0) {
+        return []
+      }
+      last = segments[0]
+      last.assignDirectLine(this.transfers[k])
+      parts.push(last)
+    }
+    return parts
+  }
+
+  searchTimes(beginTime) {
+    beginTime = 11 * 60 + 30
+    let times = []
+    let initSegTime = this.transfers[0].line.findDepartures(this.transfers[0].begin, this.transfers[0].destination, beginTime - 1)
+    for (let initSegment of initSegTime) {
+      let segments = this.segmentsForInit(initSegment)
+      if (this.transfers.length == segments.length) {
+        times.push(new TimeRoute(this, segments))
+      }
+    }
+    return times
+  }
 }
 
-class Shedule {
+// Je postaveno na route ve vztahu 1:M; Route je geograficky mozna cesta, kterou muze byt
+// mozne realizovat v ruznych casech. Pro kazdy mozny cas se vytvari instance Time Route
+class TimeRoute {
+  constructor(route, segments) {
+    this.route = route
+    this.segments = segments
+  }
+
+  totalTime() {
+    return this.segments[this.segments.length - 1].arrival - this.segments[0].departure
+  }
+
+  transfersCount() {
+    return this.route.transfers.length - 1
+  }
+
+  departure() {
+    return this.segments[0].departure
+  }
+
+  arrival() {
+    return this.segments[this.segments.length - 1].arrival
+  }
+
+  directLine(index) {
+    return this.route.getDirectLine(index)
+  }
+}
+
+// Prohledavani grafu z geografickeho pohledu a nalezani vsech moznych cesta mezi dvema mista
+// Neresi cas, hleda vsechny existujici cesty. Je mozne, ze nektere budou po spocitani casu
+// nerealizovatelne
+class GeographicalSearcher {
 
   constructor(lines) {
     this.lines = lines
@@ -115,7 +275,7 @@ class Shedule {
   searchStops(name) {
     let routes = []
     for (let line of this.lines) {
-      for (let stop of line.getStops()) {
+      for (let stop of line.getStations()) {
         if (stop.name == name) {
           routes.push(new Route(line, stop.name))
         }
@@ -125,13 +285,13 @@ class Shedule {
   }
 
   searchDirections(route) {
-    // najde vsechny mozne pokracovaci smery aktualni cestey mimo tech, ktere by se cyklily
+    // najde vsechny mozne pokracovaci smery aktualni cesty mimo tech, ktere by se cyklily
     let directions = []
     for (let line of this.lines) {
       if (route.isAtBegin() && line.name != route.getLastLine().name) {
         continue
       }
-      let stations = line.getStops()
+      let stations = line.getStations()
       let checkAndPush = (sRef, sTrg) => {
         if (stations[sRef].name == route.getLast()) {
           if (!route.containStation(stations[sTrg].name)) {
@@ -154,7 +314,7 @@ class Shedule {
       return []
     }
     let routes = this.searchStops(from)
-    let found = []
+    let found = []  // pole instanci Route
     while (routes.length > 0) {
       for (let routeIndex = routes.length - 1; routeIndex >= 0; routeIndex--) {
         let route = routes[routeIndex]
@@ -175,18 +335,6 @@ class Shedule {
       }
     }
     return found
-  }
-}
-
-class Printer {
-  printLine(line) {
-    document.write(line + '<br>')
-  }
-
-  routes(routes) {
-    for (let route of routes) {
-      this.printLine(route.str())
-    }
   }
 }
 
--- jizdni-rady/search.js
+++ jizdni-rady/search.js
@@ -1,5 +1,88 @@
 
+class Printer {
+  clear() {
+    document.getElementById('result').innerHTML = '<p><b>Spoje jsou řazeny podle času příjezdu od nejdřívějšího</b></p>'
+  }
+
+  line(line) {
+    document.getElementById('result').innerHTML += line + '<br>'
+  }
+
+  route(route) {
+    let out = '<div class="resultRoute">'
+    out += `<div class="resultCaption">Odjezd <b>${intToTime(route.departure())}</b>, příjezd <b>${intToTime(route.arrival())}</b>, `
+    out += `celkový čas <b>${intToTime(route.totalTime())}</b>, ${route.transfersCount()} přestupů</div><br>`
+
+    out += `Trasa:<br>`
+    out += '<div class="transfers">'
+    let last = undefined
+    for (let indexSegment in route.segments) {
+      let segment = route.segments[indexSegment]
+      let direct = route.directLine(indexSegment)
+
+      if (last) {
+        out += `<span class="resultTransfer">Doba na přestup ${intToTime(segment.departure - last.arrival)}</span><br>`
+      }
+
+      out += `Linka ${direct.lineName()}, odjezd v <b>${intToTime(segment.departure)}</b> z ${direct.begin}`
+      out += ` &rarr; příjezd v <b>${intToTime(segment.arrival)}</b> do ${direct.destination},`
+      out += ` doba jízdy ${intToTime(segment.totalTime)}<br>`
+      last = segment
+    }
+    out += '</div>'
+    out += '<br>'
+    out += '</div>'
+    return out
+  }
+
+  routes(routes) {
+    for (let route of routes) {
+      this.line(this.route(route))
+    }
+  }
+}
+
 printer = new Printer()
-shedule = new Shedule(lines)
-foundRoutes = shedule.search('Bites', 'Arnolec')
-printer.routes(foundRoutes)
+shedule = new GeographicalSearcher(lines)
+
+function addIntoSelect(id, item) {
+  var select = document.getElementById(id)
+  var option = document.createElement('option')
+  option.text = item
+  select.add(option)
+}
+
+allStations = []
+for (let line of lines) {
+  for (let stop of line.getStations()) {
+    allStations.push(stop.name)
+  }
+}
+allStations.sort()
+for (let station of allStations) {
+  addIntoSelect('from', station)
+  addIntoSelect('to', station)
+}
+
+function search() {
+  let from = document.getElementById('from').value
+  let to = document.getElementById('to').value
+
+  printer.clear()
+
+  if (from.startsWith('---') || to.startsWith('---')) {
+    printer.line('Vyberte začátek a konec cesty prosím.')
+  } else if (from == to) {
+    printer.line('Začátek a konec se shodují, nemusíte nikam cestovat :)')
+  } else {
+    geoRoutes = shedule.search(from, to)
+    timeRoutes = []
+    for (let geoRoute of geoRoutes) {
+      timeRoutes = timeRoutes.concat(geoRoute.searchTimes())
+    }
+    sortedTimeRoutes = timeRoutes.sort((a, b) => { return a.arrival() - b.arrival() })
+    printer.routes(sortedTimeRoutes)
+  }
+}
+
+document.getElementById('search').onclick = search
